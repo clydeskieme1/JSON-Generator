d94cc2ddd516de0c313159bfbbfc472a0e33f856
Clyde Suizo <141197487+clydeskieme1@users.noreply.github.com>
2025-10-02 04:05:47 +0800

    Edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 import EmailJsonGeneratorUI from './EmailJsonGeneratorUI.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
